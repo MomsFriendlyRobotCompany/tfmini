40f200a1b8e53bc350b6488a1e62b00c981e9356
walchko <walchko@users.noreply.github.com>
2018-02-11 16:00:36 -0700

    working :tada:

---
+++ example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# MIT License
+#
+
+from __future__ import division, print_function
+# from serial import Serial
+import time
+from tfmini import TFmini
+
+
+tf = TFmini('/dev/tty.usbserial-A506BOT5', mode=TFmini.STD_MODE)
+
+try:
+    print('='*25)
+    while True:
+        d = tf.read()
+        # d = tf.read()
+        # s = q = 0
+        if d:
+            print('Distance: {:5}'.format(d))
+        else:
+            print('No valid response')
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    tf.close()
+    print('bye!!')
--- tfmini.py
+++ tfmini.py
@@ -4,10 +4,16 @@
 #
 # TFmini appears to be an eye safe laser ... asked for confirmation
 # 120 mW @ 850 nm, class ?
+# Sparkfun website says it is not a laser ... hence, no eye issues
 #
 
 from __future__ import division, print_function
 from serial import Serial
+import time
+
+__author__ = "Kevin Walchko"
+__license__ = "MIT"
+__version__ = "0.1.0"
 
 
 class TFmini(object):
@@ -19,24 +25,38 @@ class TFmini(object):
     NOHEADER = 1
     BADCHECKSUM = 2
     TOO_MANY_TRIES = 3
+    DEC_MODE = 4
+    STD_MODE = 5
 
-    def __init__(self, port, retry=25):
+    def __init__(self, port, mode=5, retry=25):
         self.serial = Serial()
         self.serial.port = port
         self.serial.baudrate = 115200
         self.serial.timeout = 0.005
         self.serial.open()
         self.retry = retry  # how many times will I retry to find the packet header
+        self.mode = mode
+        self.strength = -1
 
         if not self.serial.is_open:
             raise Exception("ERROR: couldn't open port: {}".format(port))
 
+        self.setStdMode(self.mode)
+
+    def setStdMode(self, mode):
         # do I need this?
-        std_mode = [0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x01, 0x06]
-        data = bytearray(std_mode)
+        if mode == self.STD_MODE:
+            cmd = [0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x01, 0x06]  # hex - packet
+        elif mode == self.DEC_MODE:
+            cmd = [0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x04, 0x06]  # dec - string
+        else:
+            raise Exception("ERROR: invalid mode {}".format(mode))
+
+        self.mode = mode
+        data = bytearray(cmd)
         data = bytes(data)
         self.serial.write(data)
-
+        time.sleep(0.1)
 
     def __del__(self):
         self.close()
@@ -44,34 +64,80 @@ class TFmini(object):
     def close(self):
         self.serial.close()
 
-    def read(self):
+    def readString(self):
+        self.serial.flushInput()
+        tmp = []
+
+        d = ' '
+        while d != '\n':
+            d = self.serial.read(1)
+            tmp.append(d)
+        try:
+            tmp = tmp[:-2]  # get rid of \r\n
+            ret = float(''.join(tmp))
+        except:
+            ret = None
+        return ret
+
+    def readPacket(self):
         # it looks like it streams data, so no command to measure needed
         self.serial.flushInput()
-        
+
         # find header
-        a,b = 0,0
+        a, b = ' ', ' '
+        tmp = []
         count = self.retry
         while True:
+            tmp.append(a)
             a = b
             b = self.serial.read(1)
-            if a == 0x59 and b == 0x59:
-                break
+            if len(a) > 0 and len(b) > 0:
+                if ord(a) == 0x59 and ord(b) == 0x59:
+                    # print('found header')
+                    break
             if count == 0:
-                return (None, None, self.TOO_MANY_TRIES)
+                # print('nothing:', tmp)
+                # print('str', ''.join(tmp))
+                return None
             count -= 1
 
-        raw = self.serial.read(6)
-        pkt = [a, b] + list(map(ord, raw))
+        raw = self.serial.read(7)
+        # build a packet (array) of ints
+        pkt = [ord(a), ord(b)] + list(map(ord, raw))
+        # print('pkt', pkt)
         try:
             ret = self.processPkt(pkt)
         except Exception as e:
+            # print('bad')
             print(e)
-            ret = (None, None, None)
+            ret = None
+        return ret
+
+    def read(self):
+        """
+        This is the main read() function. The others are automagically selected
+        based off the mode the sensor was set too.
+        STD Mode: return (dist, strength, quality)
+        DEC Mode: return range in meters
+        """
+        if self.mode == self.STD_MODE:
+            ret = self.readPacket()
+        elif self.mode == self.DEC_MODE:
+            ret = self.readString()
+        else:
+            raise Exception('ERROR: read() invalid mode {}'.format(self.mode))
+
+        # weed out stupid returns ... strings can give crazy numbers
+        if ret > 12.0 or ret < 0.3:
+            ret = None
+
         return ret
 
     def processPkt(self, pkt):
         """
-        packet = [0x59, 0x59, distL, distH, strL, strH, reserved, quality, checksum]
+        packet = [0x59, 0x59, distL, distH, strL, strH, reserved, integration time, checksum]
+
+        Note: the integration time always seems to be 0
         """
         # turn string data into array of bytes
         # pkt = list(map(ord, pkt))
@@ -83,31 +149,19 @@ class TFmini(object):
             raise Exception("ERROR: bad header in packet")
 
         # calculate checksum
-        # cs = 0
-        # for b in pkt[:8]:
-        #     cs += b
         cs = sum(pkt[:8])
         cs &= 0xff
+        # print('cs', cs, pkt[8])
 
         if pkt[8] != cs:
+            print('cs', cs, pkt[8])
             raise Exception("ERROR: bad checksum in packet")
 
-        dist = pkt[2] + pkt[3]<<8
-        st   = pkt[4] + pkt[5]<<8
-        q    = pkt[7]
+        # print('L {} H {}'.format(pkt[2], pkt[3]))
+        dist = (pkt[2] + (pkt[3] << 8))/100
+        self.strength = pkt[4] + (pkt[5] << 8)
+        # q    = pkt[7]
 
-        return (dist, st, q)
+        # print('ans',dist, st, q)
 
-if __name__ == "__main__":
-    tf = TFmini('COM3')
-
-    try:
-        print('='*25)
-        while True:
-            (d, s, q) = ts.get()
-            print('Distance: {:5} Strenght: {:5} Quality: {}'.format(d, s, q))
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        tf.close()
-        print('bye!!')
+        return dist
